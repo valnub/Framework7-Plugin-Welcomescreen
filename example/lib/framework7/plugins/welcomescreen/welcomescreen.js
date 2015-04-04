fc5265a1a901fc62a7c38a0047e806e8ba4de19c
valnub <valmar@tacbook13.speedport_w_921v_1_37_000>
2015-04-04 22:17:39 +0200

    Fixes

---
+++ example/lib/framework7/plugins/welcomescreen/welcomescreen.js
@@ -0,0 +1,68 @@
+/*jslint browser: true*/
+/*global console, Framework7, alert, Dom7, Swiper*/
+
+/**
+ * A plugin for Framework7 to show a slideable welcome screen
+ *
+ * @author www.timo-ernst.net
+ * @license MIT
+ */
+Framework7.prototype.plugins.welcomescreen = function (app, globalPluginParams) {
+  'use strict';
+  
+  var Welcomescreen = function (slides, options) {
+    var self = this,
+      $$ = Dom7,
+      swiper,
+      $swiperContainer,
+      $closebtn,
+      $wscreen;
+    
+    function initSwiper() {
+      swiper = new Swiper('.swiper-container', {
+        direction: 'horizontal',
+        loop: false
+      });
+    }
+    
+    function addSlides() {
+      var i,
+        $swiperwrapper = $swiperContainer.find('.swiper-wrapper');
+      
+      for (i = 0; i < slides.length; i = i + 1) {
+        $swiperwrapper.append($$('<div class="swiper-slide">').html(slides[i].contenthtml));
+      }
+      
+      initSwiper();
+    }
+    
+    function removeWelcomeScreen() {
+      $closebtn.off('click');
+      swiper.destroy();
+      $wscreen.remove();
+    }
+    
+    // Init
+    (function () {
+      var clientLeft;
+      
+      $wscreen = $$('<div class="welcomescreen-container">');
+      $closebtn = $$('<div class="welcomescreen-closebtn">');
+      $swiperContainer = $$('<div class="welcomescreen-swiper swiper-container">').html('<div class="swiper-wrapper"></div>');
+      $wscreen.append($closebtn);
+      $wscreen.append($swiperContainer);
+      $closebtn.html('x').click(removeWelcomeScreen);
+      $$('body').append($wscreen);
+      
+      addSlides();
+      
+    }());
+    
+    return this;
+  };
+  
+  app.welcomescreen = function (options) {
+    return new Welcomescreen(options);
+  };
+  
+};
