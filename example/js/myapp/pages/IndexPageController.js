73984207b66a62fa783daf57b3fc264780d867e4
Vladimir Kharlampidi <nolimits4web@gmail.com>
2015-04-06 00:11:44 +0300

    Correct ids, remove `window` typo

--- example/js/myapp/pages/IndexPageController.js
+++ example/js/myapp/pages/IndexPageController.js
@@ -19,28 +19,29 @@ myapp.pages.IndexPageController = function (myapp, $$) {
     
     welcomescreen_slides = [
       {
-        id: 0,
+        id: 'slide0',
         picture: '<div class="tutorialicon">♥</div>',
         text: 'Welcome to this tutorial. In the next steps we will guide you through a manual that will teach you how to use this app.'
       },
       {
-        id: 1,
+        id: 'slide1',
         picture: '<div class="tutorialicon">✲</div>',
         text: 'This is slide 2'
       },
       {
-        id: 2,
+        id: 'slide2',
         picture: '<div class="tutorialicon">♫</div>',
         text: 'This is slide 3'
       },
       {
-        id: 3,
+        id: 'slide3',
         picture: '<div class="tutorialicon">☆</div>',
         text: 'Thanks for reading! Enjoy this app.<br><br><a id="tutorial-close-btn" href="#">End Tutorial</a>'
       }
+
     ];
 
-    window.welcomescreen = myapp.welcomescreen(welcomescreen_slides, options);
+    welcomescreen = myapp.welcomescreen(welcomescreen_slides, options);
     
     $$('#tutorial-close-btn').click(function () {
       welcomescreen.close();
